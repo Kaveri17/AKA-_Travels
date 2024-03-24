c93a917dbaad56bc6b8845d977dba2c47e787372
kaveripunu <kaveripunu@gmail.com>
2024-03-24 22:11:57 +0545

    Modified Triptypes & Activities pages

--- src/MyRoutes.jsx
+++ src/MyRoutes.jsx
@@ -1,22 +1,11 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './Layout/Layout'
-
-
-
-
-
-
-
 import About from './Pages/About'
-
-
 import Home from './Pages/Home'
 import Test from './Pages/Test'
-
 import Blogs from './Layout/Blogs'
 import Popular from './Layout/Popular'
-
 import Activities from './Pages/Activities'
 import TripTypes from './Pages/TripTypes'
 
@@ -28,18 +17,13 @@ const MyRoutes = () => {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Layout/>}>
-                    {/* <Route index element={<Home/>}/> */}
-                    {/* <Route index element={<Home/>}/> */}
-                 <Route index element={<Home/>}/>
-                    <Route path='./test' element={<Test/>}/>
-            
+                    <Route index element={<Home/>}/>
+                    <Route path='./test' element={<Test/>}/>            
                     <Route path='about' element={<About/>}/>
-
                     <Route path='/blogs' element={<Blogs/>}/>
                     <Route path='/popular' element={<Popular/>}/>
                     <Route path='/activity' element={<Activities/>}/>
                     <Route path='/triptypes' element={<TripTypes/>}/>
-
                 </Route>
             </Routes>
         </BrowserRouter>
