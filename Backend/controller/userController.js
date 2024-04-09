aaf4687d95a6a11590c88d6d6b1a95a852c6397d
kaveripunu <kaveripunu@gmail.com>
2024-04-09 17:18:14 +0545

    Reset Password,User Verification (day 3)

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -39,7 +39,7 @@ exports.register = async (req, res) => {
 
     // generate token
     const token = await Token.create({
-        token : crypto.randomBytes(24).toString('hex').toString('hex'),
+        token : crypto.randomBytes(24).toString('hex'),
         user: new_user._id
     })
     if(!token){
@@ -73,7 +73,7 @@ exports.verifyEmail = async (req, res) => {
     if (!user) {
         return res.status(400).json({ error: "User not found" })
     }
-// check if user is already verified
+    // check if user is already verified
     if (user.isVerified) {
         return res.status(400).json({ error: "User already verified. Login to continue" })
     }
@@ -85,3 +85,96 @@ exports.verifyEmail = async (req, res) => {
     }
     res.send({message: "User verified successfully"})
 }
+
+// to resend verification email 
+exports.resendVerification = async(req, res) => {
+    // checking if email is registered or not 
+    let user = await User.findOne({email: req.body.email})
+    if(!user){
+        return res.status(400).json({error:"Email not registered"})
+    }
+    // checking if password is correct or not
+    if(!user.authenticate(req.body.password)){
+        return res.status(400).json({error:"Email and password doesnot match"})
+    }
+    // checking if already verified 
+    if(user.isVerified){
+        return res.status(400).json({error:"User already verified.Login to continue"})
+    }
+    // generate token
+    const token = await Token.create({
+        token : crypto.randomBytes(24).toString('hex'),
+        user: user._id
+    })
+    if(!token){
+        return res.status(400).json({error:"Failed to generate token"})
+    }
+
+    // send the email verification link
+    const URL = `http://localhost:8000/verify/${token.token}`
+
+    sendEmail({
+        from: "noreply@something.com",
+        to: req.body.email,
+        subject: "Verification Email",
+        text: "Please paste the following link in the broswer to verify your email " + URL,
+        html: `<a href='${URL}'><button>Verify Email</button></a>`  
+    })
+
+    res.send({message: "Verification Link Has Been Sent To Your Email"})
+
+}
+
+// forget password
+exports.forgetpassword = async(req,res) => {
+    // check if email exist or not
+    let user = await User.findOne({email: req.body.email}) 
+    if(!user){
+        return res.status(400).json({error:"Email not registered."})
+    }
+    // generate token
+    const token = await Token.create({
+        token : crypto.randomBytes(24).toString('hex'),
+        user : user._id
+    })
+    if(!token){
+        return res.status(400).json({error:"Token not generated. Please try again later"})
+    }
+
+    // sending password reset link in email
+    const URL = `http://localhost:8000/resetpassword/${token.token}`
+
+    sendEmail({
+        from: "noreply@something.com",
+        to: req.body.email,
+        subject: "Password Reset Link",
+        text: "Please paste the following link in the broswer to reset your password " + URL,
+        html: `<a href='${URL}'><button>Reset Password</button></a>`  
+    })
+
+    res.send({message: "Password Reset Link Has Been Sent To Your Email."})
+}
+
+// to reset password
+exports.resetPassword = async (req,res) =>{
+    // checking if the token is valid or not
+    let token = await Token.findOne({token : req.params.token})
+    if(!token){
+        return res.status(400).json({error: "Token Invalid or Token Expired. Regenerate the token again."})
+    }
+    // find User associated with the token 
+    let user = await User.findById(token.user)
+    if(!user){
+        return res.status(400).json({error: "User Not Found."})
+    }
+    // updating/resetting password
+    user.password = req.body.password
+    user = await user.save()
+    if(!user){
+        return res.status(400).json({error: "Something went wrong. Please try again later"})
+    }
+    res.send({message: "Password has changed successfully."})
+
+}
+
+
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const {register, verifyEmail} = require('../controller/userController')
+const {register, verifyEmail, resendVerification, forgetpassword, resetPassword} = require('../controller/userController')
 const router = express.Router()
 
 router.post('/register', register)
 router.get('/verify/:token',verifyEmail)
+router.post('/resendverification',resendVerification)
+router.post('/forgetpassword',forgetpassword)
+router.post('/resetpassword/:token',resetPassword)
 
 module.exports = router
