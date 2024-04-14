d4a59109095c69727e66d9eba713cf0785bfea53
ealinaa <alinaadhikari108@gmail.com>
2024-04-14 13:23:48 +0545

    backend(6th day commit)

--- Backend/controller/orderController.js
+++ Backend/controller/orderController.js
@@ -14,7 +14,7 @@ exports.placeOrder = async (req, res) => {
                 
             })
             if(!ORDERITEM){
-                return red.status(400).jsob({error:"Something Went Wrong"})
+                return res.status(400).json({error:"Something Went Wrong"})
 
             }
             return ORDERITEM._id
@@ -46,13 +46,11 @@ let order_to_place = await Order.create({
       status: req.body.status,
       payment_info: req.body.payment_info
     
-
-
-
 })
 if(!order_to_place){
     return res.status(400).json({error:"Failed to place order"})
 }
+res.send(order_to_place)
 }  
 
 // const Order = require('../models/orderModel')
@@ -131,3 +129,32 @@ if(!order_to_place){
 //         return res.status(500).json({ error: "Internal server error" });
 //     }
 // };
+
+// to get order list
+
+exports.getAllOrders = async (req, res) => {
+    let orders = await Order.find()
+    .populate('user','username')
+.populate({path:'orderItems',populate:{path:"package"}})
+    if(!orders) {
+        return res.status(400).json({error:"Something Went Wrong"})
+    }
+    res.send(orders)
+
+}
+
+// to get order details
+
+exports.getOrderDetails = async (req, res) => {
+    let order = await Order.findById(req.params.id)
+    .populate('user','username')
+.populate({path:'orderItems',populate:{path:"package"}})
+    if(!order) {
+        return res.status(400).json({error:"Something Went Wrong"})
+    }
+    res.send(order)
+
+}
+    
+
+
